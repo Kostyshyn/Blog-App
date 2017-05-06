f56ee36a351ca6edef5fadba0238536da40af57e
Kostyshyn <kostyshyn@ua.fm>
2017-05-06 16:18:22 +0300

    beta v0.3

--- public/components/Home.vue
+++ public/components/Home.vue
@@ -1,16 +1,58 @@
 <template>
 	<section>
-		<h1>Home page</h1>		
+		<h1>Home page</h1>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus beatae repellat nulla tenetur delectus voluptate enim magnam, dolore quae culpa sed suscipit, consectetur architecto molestias praesentium similique molestiae sapiente laudantium.</p>
+		<hr>
+		<p>data: {{ data }}</p>	
+		<br>
+		<hr>
+		<input type="text" v-model="info">
+		<button class="button" v-on:click="sendData">Send</button>	
 	</section>
 </template>
 
+<style>
+	p {
+		color: black;
+	}
+</style>
 
 <script>
 export default {
-	data(){
-		return {}
+	data: function(){
+		return {
+			data: null,
+			info: null
+		}
 	},
 	methods: {
+		getData: function(){
+			this.$http.get('/api').then(function(response){
+
+			this.data = response.data.message;
+
+			console.log('data:', this.data);
+
+			}, function(response){
+			    // error callback
+			    console.log('error:' + response);
+			});
+		},
+		sendData: function(){
+			console.log('Info', this.info);
+
+			this.$http.post('/api', {info: this.info}).then(function(response){
+
+				console.log('sended:', response);
+
+			}, function(response){
+			    // error callback
+			    console.log('error:', response);
+			});
+		}
+	},
+	created: function(){
+		this.getData();
 	}
 }
 </script>
--- routes/api_v1.0/index.js
+++ routes/api_v1.0/index.js
@@ -8,6 +8,17 @@ const router = express.Router();
 
 // API 
 
+router.get('/api', function(req, res, next){
+	res.json({
+		message: 'Hello from API!'
+	});
+});
 
+router.post('/api', function(req, res, next){
+	var info = req.body.info;
+	console.log(info);
+	res.status(200);
+	res.send('Done');
+});
 
 module.exports = router;
